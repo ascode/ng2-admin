8f20cf0a0f4436ba9c2c3ca1685df8e5cd52a5d2
王饶冀 <2152860@qq.com>
2017-05-18 16:35:16 +0800

    添加用户信息页面

--- src/app/pages/organization/user/components/userMgmt/userMgmt.component.ts
+++ src/app/pages/organization/user/components/userMgmt/userMgmt.component.ts
@@ -1,9 +1,71 @@
 import { Component } from '@angular/core';
+import { UserMgmtService } from './userMgmt.service' 
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'user-mgmt',
-  template: `<strong>My page content here 用户管理</strong>`,
+  templateUrl: './userMgmt.html',
+  styleUrls: ['./userMgmt.scss']
 })
 export class UserMgmtComponent {
-  constructor() {}
+  query: string = '';
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="ion-ios-plus-outline"></i>',
+      createButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="ion-edit"></i>',
+      saveButtonContent: '<i class="ion-checkmark"></i>',
+      cancelButtonContent: '<i class="ion-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="ion-trash-a"></i>',
+      confirmDelete: true
+    },
+    columns: {
+      id: {
+        title: '用户姓名',
+        type: 'string'
+      },
+      firstName: {
+        title: '用户登陆名',
+        type: 'string'
+      },
+      lastName: {
+        title: '是否允许登陆',
+        type: 'string'
+      },
+      username: {
+        title: '数据状态',
+        type: 'string'
+      },
+      email: {
+        title: '有效期',
+        type: 'string'
+      },
+      age: {
+        title: '是否启用',
+        type: 'number'
+      }
+    }
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(protected service: UserMgmtService) {
+    this.service.getData().then((data) => {
+      this.source.load(data);
+    });
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
--- src/app/pages/organization/user/user.module.ts
+++ src/app/pages/organization/user/user.module.ts
@@ -2,23 +2,31 @@ import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgaModule } from '../../../theme/nga.module';
+// import { HttpModule } from "@angular/http";
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { routing } from './user.routing';
 import { UserComponent } from './user.component';
 import { UserCreateComponent } from './components/userCreate/userCreate.component';
 import { UserMgmtComponent } from './components/userMgmt/userMgmt.component';
 
+import { UserMgmtService } from './components/userMgmt/userMgmt.service'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     NgaModule,
+    Ng2SmartTableModule,
+    // HttpModule,
     routing
   ],
   declarations: [
     UserComponent,
     UserCreateComponent,
     UserMgmtComponent
+  ],
+  providers: [
+    UserMgmtService
   ]
 })
 export class UserModule {}
