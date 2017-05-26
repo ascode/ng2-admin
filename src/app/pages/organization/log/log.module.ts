6fc289365fed082bf79e5594b329d7df26835870
金飞 <28206254@qq.com>
2017-05-26 19:38:51 +0800

    s

--- src/app/pages/organization/log/log.module.ts
+++ src/app/pages/organization/log/log.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgaModule } from '../../../theme/nga.module';
 
 import { routing } from './log.routing';
 import { LogComponent } from './log.component';
@@ -13,6 +14,7 @@ import { LogForUserOperateExceptionComponent } from './components/LogForUserOper
   imports: [
     CommonModule,
     FormsModule,
+    NgaModule,
     routing
   ],
   declarations: [
