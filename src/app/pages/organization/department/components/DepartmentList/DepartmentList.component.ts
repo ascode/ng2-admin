e732496ee5e6f3ad92e98d3be5cc810e8a6a2b9c
zhangly <zhangly@example.com>
2017-06-07 11:05:00 +0800

    部门管理新增与查询接口

--- src/app/pages/organization/department/components/DepartmentList/DepartmentList.component.ts
+++ src/app/pages/organization/department/components/DepartmentList/DepartmentList.component.ts
@@ -1,75 +1,105 @@
-// import { Component, OnInit } from '@angular/core';
-// import { NodeEvent,TreeModel,Ng2TreeSettings} from 'ng2-tree';
+import { Component, OnInit } from '@angular/core';
+import { NodeEvent, TreeModel, Ng2TreeSettings } from 'ng2-tree';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { ActivatedRoute, Params } from '@angular/router';
+declare var $: any;
+export class DeList {
+  constructor(
+    public organization_code: string = '',
+    public name: string = '',
+    public brief_name: string = '',
+    public description: string = '',
+    public mnemonic_code: string = '',
+    public prd_manager_name: string = '',
+    public data_status: string = '',
+    public prd_creator_name: string = 'zzz',
+    public part_name: string = '',
+  ) { }
+}
+@Component({
+  selector: 'DepartmentList',
+  templateUrl: './DepartmentList.html',
+  styleUrls: ['./DepartmentList.scss']
+})
+export class DepartmentListComponent implements OnInit {
+  DeListData: Object;
+  public settings: Ng2TreeSettings = {
+    rootIsVisible: false
+  };
+  public pls: TreeModel;
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.pls = {
+        value: '跟节点',
+        children: [
+          {
+            value: '部门管理',
+            children: [
+              { value: 'AspectJ' },
+              { value: 'AspectC++' }
+            ]
+          },
+          {
+            value: '新建部门',
+            children: [
+              { value: 'C++' },
+              { value: 'C#' }
+            ]
+          },
+          {
+            value: '部门列表',
+            children: [
+              { value: 'JavaScript' },
+              { value: 'CoffeeScript' },
+              { value: 'TypeScript' }
+            ]
+          }
+        ],
+      };
+    }, 2000);
+  }
+  constructor(private http: Http) {
+    this.http.get('http://192.168.2.238:8000/json/reply/QueryAllDepartmentsReq').subscribe((res: Response) => {
+      console.log(res.json().departments);   
+    });
+  }
+  DeListObj = new DeList();
+  department(): void {
+    let DeList = this.DeListObj;
+    console.log(DeList)
+    this.http.post(
+      'http://192.168.2.238:8000/json/reply/CreateDepartmentReq',
+      JSON.stringify(DeList)).subscribe((res: Response) => {
+        console.log(res);
+      });
+  }
+  public onNodeRemoved(e: NodeEvent): void {
+    DepartmentListComponent.logEvent(e, 'Removed');
+    console.log(e)
+  }
 
-// // require('./ceshi.css');
+  public onNodeMoved(e: NodeEvent): void {
+    DepartmentListComponent.logEvent(e, 'Moved');
+    console.log(e)
+  }
 
-// // declare const alertify: any;
+  public onNodeRenamed(e: NodeEvent): void {
+    DepartmentListComponent.logEvent(e, 'Renamed');
+    console.log(e)
+  }
 
-// @Component({
-// 	selector: 'DepartmentList',
-// 	templateUrl: './DepartmentList.html',
-// 	styleUrls: ['./DepartmentList.scss']
-// })
-// export class DepartmentListComponent implements OnInit {
-//   public settings: Ng2TreeSettings = {
-//     rootIsVisible: false
-//   };
-//   public pls: TreeModel;
+  public onNodeCreated(e: NodeEvent): void {
+    DepartmentListComponent.logEvent(e, 'Created');
+    console.log(e)
+  }
 
-//   public ngOnInit(): void {
-//     setTimeout(() => {
-//       this.pls = {
-//         value: '跟节点',
-//         children: [
-//           {
-//             value: '部门管理',
-//             children: [
-//               { value: 'AspectJ' },
-//               { value: 'AspectC++' }
-//             ]
-//           },
-//           {
-//             value: '新建部门',
-//             children: [
-//               { value: 'C++' },
-//               { value: 'C#' }
-//             ]
-//           },
-//           {
-//             value: '部门列表',
-//             children: [
-//               { value: 'JavaScript' },
-//               { value: 'CoffeeScript' },
-//               { value: 'TypeScript' }
-//             ]
-//           }
-//         ],
-//       };
-//     }, 2000);
-//   }
+  public onNodeSelected(e: NodeEvent): void {
+    DepartmentListComponent.logEvent(e, 'Selected');
+    console.log(e)
+  }
 
-  // public onNodeRemoved(e: NodeEvent): void {
-  //   DepartmentListComponent.logEvent(e, 'Removed');
-  // }
-
-  // public onNodeMoved(e: NodeEvent): void {
-  //   DepartmentListComponent.logEvent(e, 'Moved');
-  // }
-
-  // public onNodeRenamed(e: NodeEvent): void {
-  //   DepartmentListComponent.logEvent(e, 'Renamed');
-  // }
-
-  // public onNodeCreated(e: NodeEvent): void {
-  //   DepartmentListComponent.logEvent(e, 'Created');
-  // }
-
-  // public onNodeSelected(e: NodeEvent): void {
-  //   DepartmentListComponent.logEvent(e, 'Selected');
-  // }
-
-  // private static logEvent(e: NodeEvent, message: string): void {
-  //   console.log(e);
-  //   // alertify.message(`${message}: ${e.node.value}`);
-  // }
-// }
+  private static logEvent(e: NodeEvent, message: string): void {
+    console.log(e);
+    // alertify.message(`${message}: ${e.node.value}`);
+  }
+}
