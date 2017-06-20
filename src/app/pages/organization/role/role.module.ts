06e84dd8677dff607a5c7914e1a1ade286746729
zhangly <zhangly@example.com>
2017-06-20 16:51:37 +0800

    表格添加按钮

--- src/app/pages/organization/role/role.module.ts
+++ src/app/pages/organization/role/role.module.ts
@@ -2,26 +2,45 @@ import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgaModule } from '../../../theme/nga.module';
+
+import { HttpModule } from "@angular/http";
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { routing } from './role.routing';
 import { RoleComponent } from './role.component';
 import { RoleCreateComponent } from './components/roleCreate/roleCreate.component';
 import { RolePrivilegeComponent } from './components/rolePrivilege/rolePrivilege.component';
-import { RolePeopleComponent } from './components/rolePeople/rolePeople.component';
 
+import { CustomEditorComponent } from './components/rolePeople/custom-editor.component';
+import { CustomRenderComponent } from './components/rolePeople/custom-render.component';
+import { RolePeopleComponent } from './components/rolePeople/rolePeople.component';
+import { RoleButtonViewComponent , RoleBasicExampleButtonViewComponent } from './components/rolePeople/roleButtonView';
+
+const EXAMPLES_COMPONENTS = [
+  CustomEditorComponent,
+  CustomRenderComponent,
+  RolePeopleComponent,
+  RoleButtonViewComponent,
+  RoleBasicExampleButtonViewComponent
+];
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     NgaModule,
     Ng2SmartTableModule,
+    HttpModule,
     routing
   ],
+  entryComponents: [
+    CustomEditorComponent,
+    CustomRenderComponent,
+    RoleButtonViewComponent,
+  ],
   declarations: [
-    RolePeopleComponent,
     RoleComponent,
     RoleCreateComponent,
-    RolePrivilegeComponent
+    RolePrivilegeComponent,
+    ...EXAMPLES_COMPONENTS
   ],
   providers: []
 })
